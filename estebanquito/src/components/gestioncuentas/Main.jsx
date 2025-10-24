fa73fc1304a9ab7e1419fa6534d7e4d2a6ec0fec
vcristian15 <vanegasescobarc@gmail.com>
2025-10-23 19:31:57 -0700

    Se añaden estilos

--- estebanquito/src/components/gestioncuentas/Main.jsx
+++ estebanquito/src/components/gestioncuentas/Main.jsx
@@ -30,23 +30,56 @@ import './Main.css'
                 <div id="transacciones">
                     <h1 id='titulo-transacciones'>Transacciones</h1>
                     <div id='box-transacciones'>
-                        <a href="">Enviar dinero</a>
-                        <img src="https://i.postimg.cc/pT8ZLRBY/pngwing-com-1.png" alt="" class='img10'/>
-                        <a href="">Pagos</a>
-                        <img src="https://i.postimg.cc/Hkx97Q27/pngwing-com-2.png" alt="" class='img10'/>
-                        <a href="">Retiros</a>
-                        <img src="https://i.postimg.cc/bN6Q8ZhG/pngwing-com-3.png" alt="" class='img10'/>
+
+                            <a href="" class='box1'>
+                                Transferencias
+                                <img src="https://i.postimg.cc/pT8ZLRBY/pngwing-com-1.png" alt="" class='img10'/>
+                            </a>
+                        
+                        
+                        
+                            <a href="" class='box1'>
+                                Pagos
+                                <img src="https://i.postimg.cc/Hkx97Q27/pngwing-com-2.png" alt="" class='img10'/>
+                            </a>
+                        
+
+                        
+                            <a href="" class='box1'>
+                                Retiros
+                                <img src="https://i.postimg.cc/bN6Q8ZhG/pngwing-com-3.png" alt="" class='img10'/>
+                            </a>
+                        
                     </div>
 
                 </div>
 
                 <div id="solicitudprestamo">
                     <h1 id='titulo-prestamos'>Prestamos</h1>
-                    <a href="">Simular prestamo</a>
-                    <a href="">Solicita tu prestamo</a>
-                    <a href="">Paga tu prestamo</a>
 
-                    <a href="">Mas informacion</a>
+                    <div id='box-prestamos'>
+
+                        <a href="" class='box1'>
+                            Simular prestamo
+                            <img src="https://i.postimg.cc/FzP8j0Yw/pngwing-com-4.png" alt="" class='img10'/>
+                            </a>
+
+                        <a href="" class='box1'>
+                            Solicita tu prestamo
+                            <img src="https://i.postimg.cc/SNvHR7FM/pngwing-com-5.png" alt="" class='img10'/>
+                            </a>
+
+                        <a href="" class='box1'>
+                            Paga tu prestamo
+                            <img src="https://i.postimg.cc/7LvcMHL4/pngwing-com-6.png" alt="" class='img10'/>
+                            </a>
+
+                        <a href="" class='box1'>
+                            Mas informacion
+                            <img src="https://i.postimg.cc/76wWdK7Q/pngwing-com-7.png" alt="" class='img10'/>
+                            </a>
+                    </div>
+
                 </div>
 
             </div>

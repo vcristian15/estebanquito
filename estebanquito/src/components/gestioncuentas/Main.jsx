86c5a6ae6c98bde75b515f71f1c037e9ee2ce90b
vcristian15 <vanegasescobarc@gmail.com>
2025-10-26 16:29:01 -0700

    Se actualizan componentes

--- estebanquito/src/components/gestioncuentas/Main.jsx
+++ estebanquito/src/components/gestioncuentas/Main.jsx
@@ -1,7 +1,10 @@
+import {useNavigate} from 'react-router'
 import './Main.css'    
 
     function Main(){
 
+    
+    const navigate = useNavigate()
     return(
         <div id="containermain">
 
@@ -49,24 +52,24 @@ import './Main.css'
                     <h1 id='titulo-transacciones'>Transacciones</h1>
                     <div id='box-transacciones'>
 
-                            <a href="" class='box1'>
+                            <button class='box1' onClick={() =>navigate('/Transferencias')}>
                                 Transferencias
                                 <img src="https://i.postimg.cc/pT8ZLRBY/pngwing-com-1.png" alt="" class='img10'/>
-                            </a>
+                            </button>
                         
                         
                         
-                            <a href="" class='box1'>
+                            <button class='box1'>
                                 Pagos
                                 <img src="https://i.postimg.cc/Hkx97Q27/pngwing-com-2.png" alt="" class='img10'/>
-                            </a>
+                            </button>
                         
 
                         
-                            <a href="" class='box1'>
+                            <button class='box1'>
                                 Retiros
                                 <img src="https://i.postimg.cc/bN6Q8ZhG/pngwing-com-3.png" alt="" class='img10'/>
-                            </a>
+                            </button>
                         
                     </div>
 
@@ -77,25 +80,25 @@ import './Main.css'
 
                     <div id='box-prestamos'>
 
-                        <a href="" class='box1'>
+                        <button class='box1'>
                             Simular prestamo
                             <img src="https://i.postimg.cc/FzP8j0Yw/pngwing-com-4.png" alt="" class='img10'/>
-                            </a>
+                            </button>
 
-                        <a href="" class='box1'>
+                        <button class='box1'>
                             Solicita tu prestamo
                             <img src="https://i.postimg.cc/SNvHR7FM/pngwing-com-5.png" alt="" class='img10'/>
-                            </a>
+                            </button>
 
-                        <a href="" class='box1'>
+                        <button class='box1'>
                             Paga tu prestamo
                             <img src="https://i.postimg.cc/7LvcMHL4/pngwing-com-6.png" alt="" class='img10'/>
-                            </a>
+                            </button>
 
-                        <a href="" class='box1'>
+                        <button class='box1'>
                             Mas informacion
                             <img src="https://i.postimg.cc/76wWdK7Q/pngwing-com-7.png" alt="" class='img10'/>
-                            </a>
+                            </button>
                     </div>
 
                 </div>
--- estebanquito/src/components/pagos/Pagos.jsx
+++ estebanquito/src/components/pagos/Pagos.jsx
@@ -1,9 +1,48 @@
+import {useNavigate} from 'react-router'
 import './Pagos.css'
 
 function Pagos(){
 
+    const navigate = useNavigate()
+    
     return(
-        <h1>holas</h1>
+        <div id="container">
+            <div id='box'>
+                <div id="titulo">   
+                    <h1>Transferencias</h1>
+                </div>
+
+                <div id="info">
+
+                    <div id="id">
+                        <input type="text" 
+                            placeholder="Número de cuenta"
+                            class='login-info'  
+                        />
+
+                    </div>
+
+                    <div id="email">
+
+                        <input type="text" 
+                            placeholder="$ 0"
+                            class='login-info'
+                        />
+
+                    </div>
+
+                
+                </div>
+
+                <div id="botones">
+
+                    <button id='btnCrear'>Enviar</button>
+                    <button id='btnIniciar' onClick={() => navigate(-1)}>Volver</button> 
+
+                </div>
+            </div>
+
+        </div>
     )
 
 }
